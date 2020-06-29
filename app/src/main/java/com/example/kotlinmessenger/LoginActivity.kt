df7673ce432fd67a8404a31d57357ba9d671362b
Aaron Gore <aaron.gore@definitiv.com.au>
2020-06-29 22:31:10 +0800

    Connected to firebase

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.example.kotlinmessenger
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        back_to_register_view.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
